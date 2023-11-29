e2e901c1bcec6668393c822eef0c96521cf7d5b7
bobchin <bobchin.ryu@gmail.com>
2023-11-29 13:11:40 +0900

    bicycles

---
+++ chapter03/01_bicycles.py
@@ -0,0 +1,20 @@
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print('/////////////////////////')
+print(bicycles[0])
+print(bicycles[0].title())
+
+print('/////////////////////////')
+print(bicycles[1])
+print(bicycles[3])
+
+print('/////////////////////////')
+print(bicycles[-1])
+print(bicycles[-3])
+print('/////////////////////////')
+
+message = f"私の最初の自転車は{bicycles[0].title()}でした"
+print(message)
+
