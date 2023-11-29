49ab4db1b862818a4002e29ae1f0e6732ccf185b
bobchin <bobchin.ryu@gmail.com>
2023-11-29 12:56:27 +0900

    space

---
+++ chapter02/space.py
@@ -0,0 +1,15 @@
+
+favorite_language = "python "
+print(f"'{favorite_language.rstrip()}'")
+print(f"'{favorite_language}'")
+
+
+favorite_language = "python "
+favorite_language = favorite_language.rstrip()
+print(f"'{favorite_language}'")
+
+
+favorite_language = " python "
+print(f"'{favorite_language.rstrip()}'")
+print(f"'{favorite_language.lstrip()}'")
+print(f"'{favorite_language.strip()}'")
