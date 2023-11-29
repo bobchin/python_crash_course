d461ffbd19ab99f342390839848282ced3059b4e
bobchin <bobchin.ryu@gmail.com>
2023-11-29 12:45:06 +0900

    tab_new

---
+++ chapter02/tab_newline.py
@@ -0,0 +1,12 @@
+
+print("==============================")
+print("Python")
+print("\tPython")
+
+print("==============================")
+print("Languages:\nPython\nC\nJavascript")
+
+print("==============================")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+print("==============================")
