b041042a1f1f88968053b3106b59857cd4a3c830
bobchin <bobchin.ryu@gmail.com>
2023-11-29 12:37:18 +0900

    fullname 追加

---
+++ chapter02/fullname.py
@@ -0,0 +1,13 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+print(f"Hello {full_name.title()}!")
+
+message = f"Hello {full_name.title()}!"
+print(message)
+
+# for Python < 3.6
+full_name = "{} {}".format(first_name, last_name)
+print(full_name)
