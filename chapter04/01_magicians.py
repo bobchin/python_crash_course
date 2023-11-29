2748cf68be1446f82334c1ec63d323063f5428fe
bobchin <bobchin.ryu@gmail.com>
2023-11-29 14:27:46 +0900

    magicians

---
+++ chapter04/01_magicians.py
@@ -0,0 +1,16 @@
+
+print("forループ ##############################")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+
+print("forループ2 ##############################")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}は素晴らしい手品を演じた！")
+    print(f"私は{magician.title()}の次の手品が待ちきれない。\n")
+
+print(f"みなさんありがとう。素晴らしい手品ショーでした！")
+
+
+
---
+++ chapter04/02_indent_error.py
@@ -0,0 +1,21 @@
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians:
+# print(magician)
+
+
+magicians = ['alice', 'david', 'carolina']
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}は素晴らしい手品を演じた！")
+print(f"私は{magician.title()}の次の手品が待ちきれない。\n")
+
+
+# message = "Hello Python World!"
+#     print(message)
+
+
+# magicians = ['alice', 'david', 'carolina']
+# for magician in magicians
+#     print(f"{magician.title()}は素晴らしい手品を演じた！")
+#     print(f"私は{magician.title()}の次の手品が待ちきれない。\n")
