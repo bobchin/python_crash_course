3097f91437d60625fe741317ba41ace70866846f
bobchin <bobchin.ryu@gmail.com>
2023-12-01 15:23:39 +0900

    修正

--- chapter10/remember_me.py
+++ chapter10/remember_me.py
@@ -1,20 +1,40 @@
 from os import path
 import json
 
-def greet_user():
-    """ユーザ名であいさつする。"""
-    filename = path.join(path.dirname(__file__), "username.json")
+def fullpath(filename):
+    return path.join(path.dirname(__file__), filename)
+
+def get_stored_username():
+    """保存されたユーザ名があれば取得する"""
+    filename = fullpath("username.json")
     try:
         with open(filename) as f:
             username = json.load(f)
-
     except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """新たなユーザ名の入力を促す。"""
+    username = get_stored_username()
+    if username:
+        print(f"おかえりなさい、{username}さん！")
+    else:
         username = input("あなたのお名前は？ ")
+        filename = fullpath("username.json")
         with open(filename, 'w') as f:
             json.dump(username, f)
-            print(f"戻ったときにも名前をおぼえていますよ、{username}さん！")
-    else:
+    return username
+
+def greet_user():
+    """ユーザ名であいさつする。"""
+    username = get_stored_username()
+    if username:
         print(f"おかえりなさい、{username}さん！")
+    else:
+        username = get_new_username()
+        print(f"戻ったときにも名前をおぼえていますよ、{username}さん！")
 
 
 greet_user()
