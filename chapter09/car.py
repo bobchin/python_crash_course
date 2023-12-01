fc06b71831471436d86e78726bb5e7b3d9ca24a5
bobchin <bobchin.ryu@gmail.com>
2023-12-01 11:33:49 +0900

    car を修正

--- chapter09/car.py
+++ chapter09/car.py
@@ -7,16 +7,22 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         """フォーマットされた名前を返す"""
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
 
+    def read_odometer(self):
+        """自動車の走行距離を出力する"""
+        print(f"走行距離は{self.odometer_reading}kmです。")
+
 
 if __name__ == '__main__':
-    my_new_car = Car('audi', 'a4', '')
+    my_new_car = Car('audi', 'a4', 2019)
     print(my_new_car.get_descriptive_name())
+    my_new_car.read_odometer()
 
 
 
