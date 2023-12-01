4e7c1e5d75d1eb565088c92e44e8323ad164bb8f
bobchin <bobchin.ryu@gmail.com>
2023-12-01 11:52:41 +0900

    electricを修正

--- chapter09/electric_car.py
+++ chapter09/electric_car.py
@@ -33,23 +33,39 @@ class Car:
         self.odometer_reading += km
 
 
+class Battery:
+    """電気自動車のバッテリーをモデル化したシンプルな実装例"""
+    def __init__(self, battery_size=75):
+        """バッテリーの属性を初期化する"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """バッテリーのサイズの説明文を出力する"""
+        print(f"この車のバッテリーは{self.battery_size}-kWhです。")
+
+    def get_range(self):
+        """バッテリーが提供する航続距離を示すメッセージを出力する"""
+        if self.battery_size == 75:
+            range = 420
+        elif self.battery_size == 100:
+            range = 510
+
+        print(f"この車の満充電時の航続距離は約{range}kmです。")
+
+
 class ElectricCar(Car):
     """電気自動車に特有の情報を表すクラス"""
 
     def __init__(self, make, model, year):
         """親クラスの属性を初期化する"""
         super().__init__(make, model, year)
-        self.battery_size = 75
-
-    def descriptive_battery(self):
-        """バッテリーのサイズの説明文を出力する"""
-        print(f"この車のバッテリーは{self.battery_size}-kWhです。")
+        self.battery = Battery()
 
 
 if __name__ == '__main__':
     my_tesla = ElectricCar('tesla', 'model s', 2019)
     print(my_tesla.get_descriptive_name())
-    my_tesla.descriptive_battery()
-
+    my_tesla.battery.describe_battery()
+    my_tesla.battery.get_range()
 
 
