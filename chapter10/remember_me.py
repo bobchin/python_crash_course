6a907f57d248796de457c447a1f73b2b7d60fdd7
bobchin <bobchin.ryu@gmail.com>
2023-12-01 14:27:36 +0900

    リファクタリング

--- chapter10/remember_me.py
+++ chapter10/remember_me.py
@@ -1,16 +1,20 @@
 from os import path
 import json
 
+def greet_user():
+    """ユーザ名であいさつする。"""
+    filename = path.join(path.dirname(__file__), "username.json")
+    try:
+        with open(filename) as f:
+            username = json.load(f)
 
-filename = path.join(path.dirname(__file__), "username.json")
-try:
-    with open(filename) as f:
-        username = json.load(f)
+    except FileNotFoundError:
+        username = input("あなたのお名前は？ ")
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"戻ったときにも名前をおぼえていますよ、{username}さん！")
+    else:
+        print(f"おかえりなさい、{username}さん！")
 
-except FileNotFoundError:
-    username = input("あなたのお名前は？ ")
-    with open(filename, 'w') as f:
-        json.dump(username, f)
-        print(f"戻ったときにも名前をおぼえていますよ、{username}さん！")
-else:
-    print(f"おかえりなさい、{username}さん！")
+
+greet_user()
