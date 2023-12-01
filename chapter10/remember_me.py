bbcfc04f7d6838932e992e7af424c261a68179e0
bobchin <bobchin.ryu@gmail.com>
2023-12-01 14:26:22 +0900

    修正

--- chapter10/remember_me.py
+++ chapter10/remember_me.py
@@ -1,10 +1,16 @@
 from os import path
 import json
 
-username = input("あなたのお名前は？ ")
 
 filename = path.join(path.dirname(__file__), "username.json")
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"戻ったときにも名前をおぼえていますよ、{username}さん！")
+try:
+    with open(filename) as f:
+        username = json.load(f)
 
+except FileNotFoundError:
+    username = input("あなたのお名前は？ ")
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+        print(f"戻ったときにも名前をおぼえていますよ、{username}さん！")
+else:
+    print(f"おかえりなさい、{username}さん！")
