0effe87f5593222fe6537c418123dba946f8eb67
bobchin <bobchin.ryu@gmail.com>
2023-12-01 11:34:23 +0900

    carを修正

--- chapter09/car.py
+++ chapter09/car.py
@@ -22,6 +22,7 @@ class Car:
 if __name__ == '__main__':
     my_new_car = Car('audi', 'a4', 2019)
     print(my_new_car.get_descriptive_name())
+    my_new_car.odometer_reading = 23
     my_new_car.read_odometer()
 
 
